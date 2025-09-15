6220a60d7271290b1d774bafb27d51f6783e1234
chddmltls <xognsl420@naver.com>
2025-09-15 09:59:46 +0900

    voxelfactory fuction added

--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -16,12 +16,13 @@ public class VoxelMaker : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1")) //사용자가 마우스를 클릭한다면,
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //마우스가 가진 방향으로 레이 쏘기
+            RaycastHit hitInfo = new RaycastHit(); //hit 관련 정보 저장
 
-            if (Physics.Raycast(ray, out hitInfo))
+            if (Physics.Raycast(ray, out hitInfo)) //hitInfo에 값이 들어온다면.. (out의 의미: 정보를 채워나가주는 output)
             {
-                
+                GameObject voxel = Instantiate(voxelFactory); //Instantiate -> 새로운 게임 오브젝트 만들어줌
+                voxel.transform.position = hitInfo.point; //복셀의 위치를 히트인포가 갖고있는 위치 값으로
             }
         }
     }
